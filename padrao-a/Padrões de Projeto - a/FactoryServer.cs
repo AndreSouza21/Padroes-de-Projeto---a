a11d35ba8aa82bf38cf5c2772774ba75de5a8681
AndreSouza21 <andre.souza21@unifesp.br>
2026-04-14 23:15:44 -0300

    padrao-a no novo estilo

---
+++ padrao-a/Padrões de Projeto - a/FactoryServer.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Padrões_de_Projeto___a
+{
+    public interface Notificacao
+    {
+        void Enviar(string mensagem);   
+    }
+
+    public class Email : Notificacao 
+    {
+        public void Enviar(string mensagem) 
+        {
+            Console.WriteLine("[EMAIL]: " + mensagem);
+        }
+    }
+
+    public class SMS : Notificacao
+    {
+        public void Enviar(string mensagem)
+        {
+            Console.WriteLine("[SMS]: " + mensagem);
+        }
+    }
+
+    public class Push : Notificacao
+    {
+        public void Enviar(string mensagem)
+        {
+            Console.WriteLine("[PUSH]: " + mensagem);
+        }
+    }
+
+    internal class FactoryServer
+    {
+        public static Notificacao create(string tipo) 
+        {
+            switch (tipo)
+            {
+                case "EMAIL":
+                    return new Email();                    
+                case "SMS":
+                    return new SMS();
+                case "Push":
+                    return new Push();           
+                default: return null;
+            }                
+        }
+    }
+
+    internal class ConfigGlobal
+    {
+        private ConfigGlobal() { }
+        private static ConfigGlobal config;
+
+        public string nomeAplicacao;
+        public string servidorEnvio;
+        public int qntdEnvio;
+
+
+        public static ConfigGlobal GetConfig()
+        {
+            if (config == null) 
+                config = new ConfigGlobal();
+            return config;
+        }
+
+        public void SetConfig(string nomeAplicacao, string servidorEnvio, int qntdEnvio)
+        {
+            this.nomeAplicacao = nomeAplicacao;
+            this.servidorEnvio = servidorEnvio;
+            this.qntdEnvio = qntdEnvio;
+        }
+    }
+
+}
---
+++ padrao-a/Padrões de Projeto - a/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Padrões_de_Projeto___a
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Notificacao msg = FactoryServer.create("EMAIL");
+            msg.Enviar("banana doce");
+
+            msg = FactoryServer.create("SMS");
+            msg.Enviar("melão pneu");
+
+
+            ConfigGlobal central = ConfigGlobal.GetConfig();
+            central.SetConfig("Estelionato", "Correios", 10);
+
+            Console.WriteLine($"Nome da Aplicação: {central.nomeAplicacao}\n" + $"Servidor de Envio: {central.servidorEnvio}\n" + $"Quantidade Máxima de Envios: {central.qntdEnvio}");
+        }
+    }
+}
