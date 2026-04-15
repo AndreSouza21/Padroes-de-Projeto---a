db9a477652cbbeca51ff2e28a0a143dbf05f7a01
AndreSouza21 <andre.souza21@unifesp.br>
2026-04-14 22:48:57 -0300

    atv padrao b no lugar certo agora

---
+++ padrão-b/Padrões de Projeto - a/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Padrões_de_Projeto___a
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConfigGlobal central = ConfigGlobal.GetConfig();
+            central.SetConfig("Estelionato", "Correios", 10);
+
+            Notificacao msg = FactoryServer.create("EMAIL");
+            msg.Enviar("banana doce");
+
+            msg = FactoryServer.create("SMS");
+            msg.Enviar("melão pneu");
+
+            //Console.WriteLine($"Nome da Aplicação: {central.nomeAplicacao}\n" + $"Servidor de Envio: {central.servidorEnvio}\n" + $"Quantidade Máxima de Envios: {central.qntdEnvio}");
+        }
+    }
+}
---
+++ padrão-b/Padrões de Projeto - a/Proxy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Padrões_de_Projeto___a
+{
+    internal class Proxy : Notificacao
+    {
+        private Notificacao nott;
+        private int qntdTentativa = 0;
+
+        public Proxy (Notificacao nott)
+        {
+            this.nott = nott;
+        }
+
+        public void Enviar(string mensagem)
+        {
+            ConfigGlobal config = ConfigGlobal.GetConfig();
+
+            if (config.qntdEnvio == 0)
+                config.qntdEnvio = 1;
+
+            if (qntdTentativa >= config.qntdEnvio)
+            {
+                Console.WriteLine("[BLOQUEADO]: Limite de envios atingido");
+                return;
+            }
+
+            Console.WriteLine($"[LOG]: Servidor de Envio {config.servidorEnvio}");
+            nott.Enviar(mensagem);
+        }
+    }
+}
