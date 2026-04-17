8805d9d4ec46f87f9b749983eea05e8c2db245c2
AndreSouza21 <andre.souza21@unifesp.br>
2026-04-17 15:19:14 -0300

    atv-pcd

---
+++ pcd/pcd/PCDAmazonia.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pcd
+{
+    class PCDAmazonia : Subject
+    {
+        public string Localizacao { get; }
+        private double _tempAgua;
+        private double _phAgua;
+        private double _umidadeAr;
+
+        public PCDAmazonia(string localizacao)
+        {
+            Localizacao = localizacao;
+        }
+
+        public double TempAgua
+        {
+            get => _tempAgua;
+            set { _tempAgua = value; NotifyObservers("TEMP"); }
+        }
+
+        public double PhAgua
+        {
+            get => _phAgua;
+            set { _phAgua = value; NotifyObservers("PH"); }
+        }
+
+        public double UmidadeAr
+        {
+            get => _umidadeAr;
+            set { _umidadeAr = value; NotifyObservers("UMIDADE"); }
+        }
+    }
+}
